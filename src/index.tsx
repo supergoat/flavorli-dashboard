0a79775397acfe34043116dacc4bdff181eee992
Panayiotis Nicolaou <p.nicolaou.13@gmail.com>
2019-04-13 09:37:18 +0100

    Added apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
+import {ApolloLink} from 'apollo-link';
+import {ApolloProvider} from 'react-apollo';
+// import resolvers from './resolvers';
+// import typeDefs from './typeDefs';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+
+const link = ApolloLink.from([
+  onError(({graphQLErrors, networkError}) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({message, locations, path}) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }),
+  new HttpLink({
+    uri: 'http://localhost:4000',
+    credentials: 'same-origin',
+    headers: {
+      authorization: 'Bearer ' + localStorage.getItem('flavorli-token'),
+    },
+  }),
+]);
+
+const client = new ApolloClient({
+  link,
+  cache,
+  // typeDefs,
+  // resolvers,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
