2fe724657f52429bf01a20f8f0450985ec6d020f
Panayiotis Nicolaou <p.nicolaou.13@gmail.com>
2019-04-15 23:40:34 +0100

    Added a dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/macro';
+import Colours from '../Colours';
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <NavbarWrapper>
       <OpenTimes>
         <Dot /> Open - Accepting Orders
       </OpenTimes>
-      <RestaurantInfo>
+      <RestaurantInfo onClick={() => setShowDropdown(d => !d)}>
         <RestaurantName>
           <p>BIFF'S JACK SHACK</p>
           <p>Biff Burrows</p>
         </RestaurantName>
         <Avatar />
       </RestaurantInfo>
+
+      <Dropdown showDropdown={showDropdown}>
+        <DropDownItem>Account</DropDownItem>
+        <DropDownItem>Menu</DropDownItem>
+        <DropDownItem>Order History</DropDownItem>
+        <DropDownItem>Settings</DropDownItem>
+        <DropDownItem>Log Out</DropDownItem>
+      </Dropdown>
     </NavbarWrapper>
   );
 };
@@ -53,14 +64,20 @@ const OpenTimes = styled.div`
 `;
 
 const RestaurantInfo = styled.div`
+  z-index: 1;
   display: flex;
+  justify-content: flex-end;
   align-items: center;
+  cursor: pointer;
 `;
 
 const RestaurantName = styled.div`
   text-align: right;
   margin-right: 10px;
   font-size: 14px;
+  p:last-of-type {
+    font-weight: bold;
+  }
 `;
 
 const Avatar = styled.div`
@@ -68,4 +85,36 @@ const Avatar = styled.div`
   height: 30px;
   border-radius: 15px;
   background: var(--osloGrey);
+`;
+
+interface DropdownProps {
+  showDropdown?: boolean;
+}
+const Dropdown = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  width: 250px;
+  background: ${Colours.white};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  padding: 70px 25px 15px;
+  transition: transform 200ms;
+  transform: ${(props: DropdownProps) =>
+    props.showDropdown ? 'translateX(0)' : 'translateX(110%)'};
+`;
+
+const DropDownItem = styled.li`
+  padding: 15px 0;
+  user-select: none;
+
+  &:last-of-type {
+    margin-top: 30px;
+    text-align: right;
+    width: 100%;
+    border-top: 1px solid var(--gallery);
+  }
 `;
