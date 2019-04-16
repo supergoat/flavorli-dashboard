ce44099b4d1215b85e84fdeee522c3e2f40a2182
Panayiotis Nicolaou <p.nicolaou.13@gmail.com>
2019-04-16 16:16:36 +0100

    Moved the sidebar and navbar to the order view

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import {RouteComponentProps, Router} from '@reach/router';
-import styled from 'styled-components/macro';
-import SideBar from './components/SideBar';
-import Navbar from './components/Navbar';
 import Order from './views/Order';
 
 interface Props extends RouteComponentProps {}
 const App = (_: Props) => {
   return (
-    <AppWrapper>
-      <Navbar />
-      <SideBar />
-
-      <RouterWrapper>
-        <Order path="/order" />
-      </RouterWrapper>
-    </AppWrapper>
+    <Router>
+      <Order path="/order" />
+    </Router>
   );
 };
 
 export default App;
-
-const AppWrapper = styled.div`
-  display: flex;
-`;
-
-const RouterWrapper = styled(Router)`
-  margin-top: 61px;
-  flex: 1;
-`;
--- src/views/Order.tsx
+++ src/views/Order.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import {RouteComponentProps} from '@reach/router';
 import styled from 'styled-components/macro';
 import Order from '../components/Order';
+import SideBar from '../components/SideBar';
+import Navbar from '../components/Navbar';
 
 interface Props extends RouteComponentProps {}
 
 const OrderView = (_: Props) => {
   return (
     <OrderWrapper>
+      <Navbar />
+      <SideBar />
+
       <Order />
     </OrderWrapper>
   );
@@ -20,4 +25,5 @@ const OrderWrapper = styled.div`
   justify-content: center;
   padding-bottom: 50px;
   width: 100%;
+  margin-top: 61px;
 `;
